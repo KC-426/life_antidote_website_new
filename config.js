b8d1a8ea60dcc37a6a41a6914fe7cc5de155fe8d
KC-426 <kuldeepchahar426@gmail.com>
2023-09-21 23:03:22 +0530

    live

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // export const baseUrl = "https://lifeantidote-backend.onrender.com";
 // export const baseUrl = "http://localhost:5000";
-// export const baseUrl = "https://life-antidote-backend.onrender.com";
-export const baseUrl = "https://urchin-app-gm72o.ondigitalocean.app";
+export const baseUrl = "https://life-antidote-backend.onrender.com";
