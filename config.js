69375155752e0134c07964cc976aa1ac97ac6b87
Kuldeep Chahar <kuldeepchahar426@gmail.com>
2023-10-14 17:03:04 -0700

    l

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // export const baseUrl = "https://lifeantidote-backend.onrender.com";
 // export const baseUrl = "http://localhost:5000";
-// export const baseUrl = "https://life-antidote-backend-x1am.onrender.com";
-export const baseUrl = "https://life-antidote-backend.onrender.com";
+export const baseUrl = "https://life-antidote-backend-x1am.onrender.com";
+// export const baseUrl = "https://life-antidote-backend.onrender.com";
